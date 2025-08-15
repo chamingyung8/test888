e0f56d66cd86b072b24da4381dc993cee1c562bc
chamingyung8 <chamingyung1@gmail.com>
2025-08-15 06:00:12 +0000

    앱 만들기 시작!

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 Welcome cha! ^^ ") # 앱 제목
+st.info("안녕하세요, cha의 파이콘 튜토리얼 연습입니다")
+st.subheader("첫번째 앱")
+
+# st.image("https://static.streamlit.io/examples/cat.jpg",
+#  caption="귀여운 고양이", use_container_width=True)
+
+st.code("""
+import python""")
+st.link_button("네이버 바로가기", "https://www.naver.com")
